ffa9ed7b734afb3df2cd5345bcb49f48540fb23a
paulning <1942681896@qq.com>
2025-05-31 01:30:58 +0800

    fix include/linux/atomic/atomic-instrumented.h

--- include/linux/atomic/atomic-instrumented.h
+++ include/linux/atomic/atomic-instrumented.h
@@ -1359,7 +1359,6 @@ atomic_sub_and_test(int i, atomic_t *v)
 }
 
 /**
-减少1 , 然后看看是不是变0了
  * atomic_dec_and_test() - atomic decrement and test if zero with full ordering
  * @v: pointer to atomic_t
  *
